dc9f231655fd583c2a8ffdf8e2463d2674e16009
Quach Cong Hoang <mr_doraemon_meomeo@yahoo.com>
2015-04-02 21:36:09 +0700

    Initial Commit

---
+++ PhysicsModelBase/PhysicsModelBase/PhysicsModelBase.cpp
@@ -0,0 +1,17 @@
+// PhysicsModelBase.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "conio.h"
+#include "iostream"
+
+#include "GLFW/glfw3.h"
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "hello\n";
+	_getch();
+	return 0;
+}
