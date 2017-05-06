da6c5e0181a976dc11020b61c085817b5175c85a
Quach Cong Hoang <mr_doraemon_meomeo@yahoo.com>
2017-05-06 18:45:21 +0700

    GOGOGO

--- RGBD-LaneDetector/Tests/Test_TemplateMatching.cpp
+++ RGBD-LaneDetector/Tests/Test_TemplateMatching.cpp
@@ -13,7 +13,9 @@ int jumpStep = 30;
 void colorizeDepthImage(const cv::Mat depthImg, cv::Mat & resultMat);
 void depthMask(const cv::Mat & depthImg, cv::Mat & mask);
 cv::Rect safeCreateRect(cv::Point tl_point, cv::Size imgSize, cv::Size preferedSize);
-float pcaAnalyzing(cv::Mat & tmpRS);
+cv::Vec2f pcaAnalyzing(cv::Mat & tmpRS);
+void getLinePoints_SlindingBox(cv::Mat & tmpResult, std::vector<cv::Point> & listPoints, cv::Vec2f pca_rs,
+	cv::Point initPoint, cv::Size boxSize = cv::Size(32,32), float jumpStep = 32);
 
 //1. Template matching: 3 template
 
@@ -21,7 +23,7 @@ float pcaAnalyzing(cv::Mat & tmpRS);
 
 //3. Polyline fitting: projection and fitting
 
-cv::Mat tmp_left, tmp_right;
+cv::Mat tmp_left, tmp_right, tmp_right_def;
 cv::Mat tmp_left_s, tmp_right_s;
 
 void generateTemplate()
@@ -31,6 +33,9 @@ void generateTemplate()
 	tmp_right = cv::Mat(templateSize, CV_8UC1, cv::Scalar(0));
 	tmp_left_s = cv::Mat(templateSize, CV_8UC1, cv::Scalar(0));
 	tmp_right_s = cv::Mat(templateSize, CV_8UC1, cv::Scalar(0));
+
+	tmp_right_def = cv::Mat(templateSize, CV_8UC1, cv::Scalar(0));
+	cv::line(tmp_right_def, cv::Point(0, 0), cv::Point(32, 32), cv::Scalar(255), 8);
 
 	cv::line(tmp_right, cv::Point(0, 0), cv::Point(32, 32), cv::Scalar(255), 8);
 	cv::line(tmp_left, cv::Point(0, 32), cv::Point(32, 0), cv::Scalar(255), 8);
@@ -51,12 +56,12 @@ void templateProcessing(cv::Mat & imgBin, const cv::Mat &tmp, cv::Mat & matching
 	cv::matchTemplate(imgBin, tmp, matchingResult, match_method);
 
 	//cv::GaussianBlur(matchingResult, matchingResult, cv::Size(5, 5), 0, 0);
-	cv::normalize(matchingResult, matchingResult, 0, 1, cv::NORM_MINMAX, -1);
+	//cv::normalize(matchingResult, matchingResult, 0, 1, cv::NORM_MINMAX, -1);
 	//cv::threshold(matchingResult, matchingResult, 0.7, 1, CV_THRESH_BINARY);
 	cv::imshow(id, matchingResult);
 }
 
-cv::Point getBestMatchLoc(const cv::Mat & matchingResult, double & maxVal, std::string id = "Matching")
+cv::Point getBestMatchLoc(const cv::Mat & matchingResult, double & maxVal, cv::Vec2f & orientVal, std::string id = "Matching")
 {
 	double minVal;
 	cv::Point minLoc, maxLoc;
@@ -67,11 +72,11 @@ cv::Point getBestMatchLoc(const cv::Mat & matchingResult, double & maxVal, std::
 	//cv::normalize(tmp, tmp, 0, 1, cv::NORM_MINMAX, -1);
 	//cv::threshold(tmp, tmp, 0.7, 1, CV_THRESH_BINARY);
 	cv::imshow(id, tmp);
-	pcaAnalyzing(tmp);
+	orientVal = pcaAnalyzing(tmp);
 	return maxLoc;
 }
 
-float pcaAnalyzing(cv::Mat & tmpRS)
+cv::Vec2f pcaAnalyzing(cv::Mat & tmpRS)
 {
 	cv::normalize(tmpRS, tmpRS, 0, 1, cv::NORM_MINMAX, -1);
 	std::vector<cv::Point2d> pcaData;
@@ -103,8 +108,77 @@ float pcaAnalyzing(cv::Mat & tmpRS)
 			pca_analysis.eigenvectors.at<double>(i, 1));
 		eigen_val[i] = pca_analysis.eigenvalues.at<double>(i, 0);
 	}
-	std::cout << eigen_vecs << " \n " << eigen_val[0] << "-" << eigen_val[1] <<"\n == \n";
-	return 0;
+	//std::cout << eigen_vecs << " \n " << eigen_val[0] << "-" << eigen_val[1] <<"\n == \n";
+	return cv::Vec2f(eigen_vecs[0].x,eigen_vecs[0].y);
+}
+
+int countNonZeroCenter(cv::Mat & _map, cv::Point & center)
+{
+	center = cv::Point(0, 0);
+	//cv::imshow("MapTMP", _map);
+	//cv::waitKey();
+	int count = 0;
+	for (int i = 0; i < _map.rows; i++)
+	{
+		for (int j = 0; j < _map.cols; j++)
+		{
+			if (_map.at<float>(i,j) > 0.1)
+			{
+				count++;
+				center += cv::Point(j, i);
+			}
+		}
+	}
+	if (count!=0)
+	{
+		center /= count;
+	}
+	return count;
+}
+
+void getLinePoints_SlindingBox(cv::Mat & tmpResult, std::vector<cv::Point> & listPoints, cv::Point initPoint, cv::Vec2f pca_rs,
+	cv::Size boxSize /* = cv::Size(32,32) */, float jumpStep /* = 32 */)
+{
+	cv::normalize(tmpResult, tmpResult, 0, 1, cv::NORM_MINMAX, -1);
+	cv::threshold(tmpResult, tmpResult, 0.2, 1, CV_THRESH_BINARY);
+
+	cv::Rect rsRect = cv::Rect(0, 0, tmpResult.cols, tmpResult.rows);
+	cv::Point jumpDistance(jumpStep * fabs(pca_rs[0]), jumpStep * fabs(pca_rs[1]));
+
+	//Go Down --||--
+	cv::Point boxTL = initPoint;
+	cv::Point boxBR = initPoint + cv::Point(jumpStep, jumpStep);
+	while (rsRect.contains(boxBR) && rsRect.contains(boxTL))
+	{
+		cv::Rect newBox(boxTL, boxBR);
+		cv::Point tmpCenter;
+		int nonZero = countNonZeroCenter(tmpResult(newBox), tmpCenter);
+		if (nonZero < 20) {break;}
+		else{
+			listPoints.push_back(tmpCenter+boxTL);
+			boxTL += tmpCenter;
+			boxBR = boxTL + jumpDistance;
+		}
+	}
+
+	//Go Up --||--
+	boxTL = initPoint - jumpDistance;
+	boxBR = initPoint;
+	while (rsRect.contains(boxBR) && rsRect.contains(boxTL))
+	{
+		cv::Rect newBox(boxTL, boxBR); 
+		cv::Point tmpCenter;
+		int nonZero = countNonZeroCenter(tmpResult(newBox), tmpCenter);
+		if (nonZero < 20){
+			break;
+		}
+		else{
+			listPoints.push_back(tmpCenter + boxTL);
+			boxTL -= tmpCenter;
+			boxBR = boxTL + jumpDistance;
+		}
+	}
+		
 }
 
 void fillRegion(cv::Mat & dMat, cv::Mat & rgbMat, cv::Rect search_rect)
@@ -125,6 +199,8 @@ void fillRegion(cv::Mat & dMat, cv::Mat & rgbMat, cv::Rect search_rect)
 	}
 }
 
+
+int preferedThresh = 128;
 void laneLineSampling(cv::Mat & rgbimg, cv::Mat & dimg)
 {
 	cv::Point op(0, 80);
@@ -134,26 +210,54 @@ void laneLineSampling(cv::Mat & rgbimg, cv::Mat & dimg)
 	cv::Mat imgGray, imgBin;
 	cv::cvtColor(img, imgGray, cv::COLOR_RGB2GRAY);
 	cv::imshow("imgGray", imgGray);
-	cv::threshold(imgGray, imgBin, 150, 255, CV_THRESH_TOZERO); //CV_THRESH_OTSU //CV_THRESH_TOZERO
+	cv::threshold(imgGray, imgBin, preferedThresh, 255, CV_THRESH_TOZERO); //CV_THRESH_OTSU //CV_THRESH_TOZERO
 	cv::imshow("imgBin", imgBin);
 	//cv::waitKey();
 
 	//cv::Mat dMask = cv::Mat(dimg.rows, dimg.cols, CV_8UC1, cv::Scalar(0));
 	//depthMask(dimg, dMask);
 
-	cv::Mat result_left, result_right, result_prev;
+	cv::Mat result_left, result_right, result_right_enhance;
 	templateProcessing(imgBin, tmp_left, result_left,"RS_L");
 	templateProcessing(imgBin, tmp_right, result_right, "RS_R");
 
 	double maxVal_Left, maxVal_Right;
-	cv::Point matchLocLeft = getBestMatchLoc(result_left, maxVal_Left,"Left_TMP") + op;
-	cv::Point matchLocRight = getBestMatchLoc(result_right, maxVal_Right, "Right_TMP") + op;
-	cv::Rect tmpRect_Left = cv::Rect(matchLocLeft, cv::Size(32,32));
-	cv::Rect tmpRect_Right = cv::Rect(matchLocRight, cv::Size(32, 32));
+	cv::Vec2f orient_Left, orient_Right;
+	cv::Point matchLocLeft = getBestMatchLoc(result_left, maxVal_Left, orient_Left, "Left_TMP") ;
+	cv::Point matchLocRight = getBestMatchLoc(result_right, maxVal_Right, orient_Right, "Right_TMP");
+	
+	tmp_right_s = cv::Mat(templateSize, CV_8UC1, cv::Scalar(0));
+	if (maxVal_Right > 0.5)
+	{
+		tmp_right = tmp_right_s;
+		//Adjust Thresh
+		/*cv::Point center_tmp = matchLocRight + cv::Point(16, 16);
+		int color = imgGray.at<uchar>(center_tmp)+
+			imgGray.at<uchar>(center_tmp - cv::Point(2, 0)) +
+			imgGray.at<uchar>(center_tmp - cv::Point(0, 2)) +
+			imgGray.at<uchar>(center_tmp - cv::Point(-2, 0)) +
+			imgGray.at<uchar>(center_tmp - cv::Point(0, -2));
+		preferedThresh = color / 5-10*/;
+		cv::Mat curr_gray = imgGray(safeCreateRect(matchLocRight + cv::Point(16, 16), cv::Size(imgGray.cols, imgGray.rows), cv::Size(32, 32)));
+		cv::imshow("Gray", curr_gray);
+	}
+	else
+	{
+		tmp_right = tmp_right_def;
+		preferedThresh = 128;
+	}
+	cv::line(tmp_right_s, cv::Point(0, 0), cv::Point(60 * fabs(orient_Right[0]), 60 * fabs(orient_Right[1])), cv::Scalar(255), 4);
+	cv::imshow("tmp _RHS", tmp_right_s);
+	templateProcessing(imgBin, tmp_right_s, result_right_enhance, "RS_R_Enhance");
 
+	std::vector<cv::Point> rightList;
+	getLinePoints_SlindingBox(result_right_enhance, rightList, matchLocRight - cv::Point(16, 16), orient_Right, cv::Size(32, 32), 32);
 	//fillRegion(dimg, rgbimg, tmpRect_Left);
 	//fillRegion(dimg, rgbimg, tmpRect_Right);
-
+	matchLocLeft += (op);
+	matchLocRight += (op - cv::Point(16, 16));
+	cv::Rect tmpRect_Left = cv::Rect(matchLocLeft, cv::Size(32, 32));
+	cv::Rect tmpRect_Right = cv::Rect(matchLocRight, cv::Size(32, 32));
 	cv::rectangle(rgbimg, tmpRect_Left, cv::Scalar(0, 255, 0), 2);
 	cv::rectangle(rgbimg, tmpRect_Right, cv::Scalar(255, 0, 0), 2);
 
@@ -164,6 +268,21 @@ void laneLineSampling(cv::Mat & rgbimg, cv::Mat & dimg)
 	//cv::threshold(result_right, result_right, 0.8, 1, CV_THRESH_BINARY);
 	//cv::imshow("Left", result_left);
 	//cv::imshow("Right", result_right);
+
+	int colorGray = 0;
+	for (int i = 0; i < rightList.size(); i++)
+	{
+		cv::circle(rgbimg, rightList[i] + op, 3, cv::Scalar(0,0,255),2);
+		colorGray += imgGray.at<uchar>(rightList[i]);
+	}
+	if (rightList.size()!=0)
+	{
+		preferedThresh = colorGray / rightList.size() - 5;
+	}
+	else
+	{
+		preferedThresh = 128;
+	}
 
 	cv::putText(rgbimg, std::to_string(count), cv::Point(500, 420), cv::HersheyFonts::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0, 0, 255), 2);
 	cv::putText(rgbimg, std::to_string(maxVal_Left), matchLocLeft - cv::Point(20, 5), cv::HersheyFonts::FONT_HERSHEY_SIMPLEX, 0.3, cv::Scalar(0, 255, 0), 1);
