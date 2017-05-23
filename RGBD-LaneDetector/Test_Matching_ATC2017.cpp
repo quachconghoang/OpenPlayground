7ef26b1fc0596e6308392fc8aae4b6e1f52e41ce
Quach Cong Hoang <mr_doraemon_meomeo@yahoo.com>
2017-05-23 09:37:24 +0700

    RGBD-Normal

--- RGBD-LaneDetector/Test_Matching_ATC2017.cpp
+++ RGBD-LaneDetector/Test_Matching_ATC2017.cpp
@@ -2,6 +2,7 @@
 #include "ImgProc/ImgProc.h"
 #include "ImgProc/cuda/ImgProcCuda.h"
 #include "ImgProc/DataIO.h"
+#include "opencv2/rgbd.hpp"
 
 int64 st, et;
 
@@ -11,77 +12,50 @@ int64 st, et;
 #define CV_COLOR_GREEN cv::Scalar(0, 255, 0)
 #define CV_COLOR_BLUE cv::Scalar(255, 0, 0)
 
-#define D_BETA 0.2f
-
-
 using namespace cv;
 
 cv::Size templateSize(32, 32);
 cv::Point templateCenter(16, 16);
 
  //Synthetic data params
-//std::string dirPath = "D:/LaneData/SynthDataLane/SEQS-01-SUMMER/";
-//int count = 200;
-//bool needResize = true;
-//cv::Size fullImg_Size(1280, 760);
-//cv::Size processImg_Size(640, 380);
-//cv::Rect laneRegion(0, 180, 640, 200);
-//cv::Point orgTemplate(0 + 16, 180 + 16);
+std::string dirPath = "D:/LaneData/SynthDataLane/SEQS-06-FOG/";
+int count = 400;
+bool needResize = true;
+cv::Size fullImg_Size(1280, 760);
+cv::Size processImg_Size(640, 380);
+cv::Rect laneRegion(0, 156, 640, 224);
+cv::Point orgTemplate(0 + 16, 156 + 16);
 
 // Real data params
-std::string dirPath = "D:/LaneData/Sample_30-04/";
-int count = 1800;
-bool needResize = false;
-cv::Size fullImg_Size(640, 480);
-cv::Size processImg_Size(640, 480);
-cv::Rect laneRegion(0, 100, 640, 380);
-cv::Point orgTemplate(0 + 16, 100 + 16);
+//std::string dirPath = "D:/LaneData/Sample_30-04/";
+//int count = 1800;
+//bool needResize = false;
+//cv::Size fullImg_Size(640, 480);
+//cv::Size processImg_Size(640, 480);
+//cv::Rect laneRegion(0, 100, 640, 380);
+//cv::Point orgTemplate(0 + 16, 100 + 16);
+//ImgProc3D::Intr m_camInfo(ImgProc3D::IntrMode_Realsense_RAW);
 
 cv::Point toOriginal(cv::Point p)	{ return p + laneRegion.tl() + templateCenter; }
 cv::Rect toOriginal(cv::Rect r)		{ return cv::Rect(r.tl() + laneRegion.tl() + templateCenter, r.size()); }
-cv::Size matchResult_Size(laneRegion.width - templateSize.width + 1,
+cv::Size matchResult_Size(
+	laneRegion.width - templateSize.width + 1,
 	laneRegion.height - templateSize.height + 1);
-
-void computeGeometricMap(cv::Mat & dImage_Meter, cv::Mat & resultMap)
-{
-	resultMap = cv::Mat(matchResult_Size, CV_32FC1, cv::Scalar(0));
-	for (int i = 0; i < resultMap.rows; i++)
-	{
-		for (int j = 0; j < resultMap.cols; j++)
-		{
-			cv::Point rsPoint(j, i);
-			cv::Point dPoint = rsPoint + orgTemplate;
-
-			float d = dImage_Meter.at<float>(dPoint);
-			if (d!=0 && d < 10){
-				resultMap.at<float>(rsPoint) = D_BETA*((10-d) / 10);
-			}
-			else{
-				resultMap.at<float>(rsPoint) = 0.5f * D_BETA*( float(i) / resultMap.rows);
-			}
-		}
-	}
-}
-
-void normalGrid(cv::Mat & xyzMat)
-{
-	cv::Size _grid;
-}
 
 struct MatchingKernel
 {
-	cv::Mat left, right, center;
+	cv::Mat left, right;
+	cv::Mat left_enhance;
+	cv::Mat right_enhance;
 };
 
 void generateTemplate(MatchingKernel & kernel)
 {
 	kernel.left = cv::Mat(templateSize, CV_8UC1, cv::Scalar(0));
 	kernel.right = cv::Mat(templateSize, CV_8UC1, cv::Scalar(0));
-	//kernel.center = cv::Mat(templateSize, CV_8UC1, cv::Scalar(0));
 	
 	cv::line(kernel.left, cv::Point(0, 32), cv::Point(32, 0), cv::Scalar(255), 12);
 	cv::line(kernel.right, cv::Point(0, 0), cv::Point(32, 32), cv::Scalar(255), 12);
-	//cv::line(kernel.center, cv::Point(16, 0), cv::Point(16, 32), cv::Scalar(255), 10);
 
 }
 
@@ -93,17 +67,61 @@ void displayKernel(MatchingKernel & kernel)
 	cv::imshow("K", displayMat);
 }
 
+#define D_BETA 0.2f
+#define  depthThresh 1000
+void computeGeometricMap(cv::Mat & dImage_Meter, cv::Mat & resultMap)
+{
+	resultMap = cv::Mat(matchResult_Size, CV_32FC1, cv::Scalar(0));
+	for (int i = 0; i < resultMap.rows; i++){
+		for (int j = 0; j < resultMap.cols; j++){
+			cv::Point rsPoint(j, i);
+			float d = float(dImage_Meter.at<ushort>(rsPoint + orgTemplate));
+			if (d != 0 && d < depthThresh){ resultMap.at<float>(rsPoint) = D_BETA*((depthThresh - d) / depthThresh); }
+			else{	resultMap.at<float>(rsPoint) = D_BETA*(float(i) / resultMap.rows);}
+		}
+	}
+}
+
+void compute_Normal_RespondMap(cv::Mat & normalMap, cv::Mat & resultMap)
+{
+	resultMap = cv::Mat(matchResult_Size, CV_32FC1, cv::Scalar(0));
+	for (int i = 0; i < resultMap.rows; i++)
+	{
+		for (int j = 0; j < resultMap.cols; j++)
+		{
+			cv::Point rsPoint(j, i);
+			cv::Vec3f n = normalMap.at<cv::Vec3f>(rsPoint + orgTemplate);
+			if (!std::isnan(n[2]))
+			{
+				resultMap.at<float>(rsPoint) = D_BETA*fabs(-1 + 2*n.dot(cv::Vec3f(0, 1, 0)));
+			}
+		}
+	}
+}
+
+void cpu_matchingTemplates()
+{}
+
 int main()
 {
 	std::vector<SyncFrame> dataHeaders;
 	readSyncFileHeader(dirPath + "associations.txt", dataHeaders);
-	ImgProc3D::Intr m_camInfo = ImgProc3D::Intr(ImgProc3D::IntrMode_Realsense_RAW);
+
+	ImgProc3D::Intr m_camInfo = ImgProc3D::Intr(ImgProc3D::IntrMode_Synthia_RGBD_HALF);
+	cv::Mat cameraMatrix = (cv::Mat_<float>(3, 3) << m_camInfo.fx, 0, m_camInfo.cx, 0, m_camInfo.fy, m_camInfo.cy, 0, 0, 1);
+	cv::rgbd::RgbdNormals n_estimate(processImg_Size.height, 
+		processImg_Size.width, 
+		CV_32F, cameraMatrix, 5, 
+		cv::rgbd::RgbdNormals::RGBD_NORMALS_METHOD_SRI);
 
 	MatchingKernel mKernel;
 	generateTemplate(mKernel);
 	displayKernel(mKernel);
+
+
 	//cv::imshow("K", mKernel.left_grad);
-	cv::waitKey();
+	//cv::waitKey();
+	
 	while (count < dataHeaders.size() - 1)
 	{
 		cv::Mat img = cv::imread(dirPath + dataHeaders[count].rgbImg/*"../Data/58-rgb.png"*/);
@@ -113,38 +131,43 @@ int main()
 			cv::resize(dimg, dimg, processImg_Size, 0, 0, cv::INTER_NEAREST);
 		}
 
-		cv::Mat dImgMeter, depthRSMap;
-		dimg.convertTo(dImgMeter, CV_32FC1);
-		dImgMeter /= m_camInfo.scale;
+		cv::cuda::GpuMat dev_dMat(dimg);
+		cv::cuda::GpuMat dev_xyzMap(dimg.rows, dimg.cols, CV_32FC3);
 
-		computeGeometricMap(dImgMeter, depthRSMap);
+		ImgProc3D::convertTo_Point3fMap(dev_dMat, m_camInfo, dev_xyzMap);
+		cv::Mat xyzMap, normalMap;
+		dev_xyzMap.download(xyzMap);
 
+		n_estimate(xyzMap, normalMap);
+		cv::Mat rsDMap,rsDMap_2;
+		compute_Normal_RespondMap(normalMap, rsDMap);
+		computeGeometricMap(dimg, rsDMap_2);
+
+		cv::Mat imgProc = img(laneRegion);
 		cv::Mat imgGray, imgBin;
-		cv::cvtColor(img, imgGray, cv::COLOR_RGB2GRAY);
-		cv::threshold(imgGray, imgBin, 128, 255, CV_THRESH_TOZERO); //CV_THRESH_OTSU //CV_THRESH_TOZERO
-		imgBin = imgBin(laneRegion);
+		cv::cvtColor(imgProc, imgGray, cv::COLOR_RGB2GRAY);
+		cv::threshold(imgGray, imgBin, 100, 255, CV_THRESH_TOZERO); //CV_THRESH_OTSU //CV_THRESH_TOZERO
 		
 		cv::Mat match_right, match_left;
 		cv::matchTemplate(imgBin, mKernel.right, match_right, CV_TM_CCOEFF_NORMED);
-
+		cv::matchTemplate(imgBin, mKernel.left, match_left, CV_TM_CCOEFF_NORMED);
 		
-		double right_MaxVal;
-		cv::Point right_MaxLoc = cpu_findMinmax(match_right, right_MaxVal);
-		match_right += depthRSMap;
-		cv::Point test_MaxLoc = cpu_findMinmax(match_right, right_MaxVal);
+		//cv::Mat geoMat;computeGeometricMap(dimg, geoMat);
+
+		double right_MaxVal,left_MaxVal;
+		cv::Point right_MaxLoc = cpu_findMinmax(match_right + rsDMap + rsDMap_2, right_MaxVal);
+		cv::Point left_MaxLoc = cpu_findMinmax(match_left + rsDMap + rsDMap_2, left_MaxVal);
 
 		cv::circle(img, toOriginal(right_MaxLoc), 7, CV_COLOR_RED, 2);
-		cv::circle(img, toOriginal(test_MaxLoc), 5, CV_COLOR_GREEN, 2);
+		cv::circle(img, toOriginal(left_MaxLoc), 7, CV_COLOR_BLUE, 2);
+
 		cv::putText(img, std::to_string(count), cv::Point(500, 300), cv::HersheyFonts::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0, 0, 255), 2);
 
 		cv::imshow("color", img);
-		cv::imshow("depth", dimg*3);
-		cv::imshow("BIN", imgBin);
+		cv::imshow("gray", imgBin);
+		cv::imshow("norm", rsDMap);
 
-		depthRSMap.convertTo(depthRSMap, CV_8U, -255.0 / 0.2, 255.0);
-		cv::applyColorMap(depthRSMap, depthRSMap, cv::COLORMAP_OCEAN);
-		cv::imshow("rsX", depthRSMap);
-		cv::imshow("rs", match_right);
+		cv::imshow("rs", match_left + rsDMap + rsDMap_2);
 		char key = cv::waitKey();
 		if (key == 27) break;
 		if (key == 'p') cv::waitKey();
